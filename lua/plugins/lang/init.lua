65ff9d29dd4abcad7c7ea4da7ce66cae318d4ed2
Michael van der Loo <michael.vanderloo@ll.mit.edu>
2025-03-31 11:33:25 -0400

    csvview keybindings

--- lua/plugins/lang/init.lua
+++ lua/plugins/lang/init.lua
@@ -96,4 +96,41 @@ return {
       }
     end,
   },
+  {
+    'hat0uma/csvview.nvim',
+    ft = { 'csv', 'tsv' },
+    opts = {
+      view = {
+        min_column_width = 1,
+        spacing = 0,
+        display_mode = 'border',
+      },
+      delimiter = {
+        default = ',',
+        ft = {
+          tsv = '\t',
+        },
+      },
+    },
+    keys = {
+      {
+        '<localleader>e',
+        '<cmd>CsvViewEnable<cr>',
+        ft = { 'csv', 'tsv' },
+        desc = 'Enable Tabular View',
+      },
+      {
+        '<localleader>d',
+        '<cmd>CsvViewDisable<cr>',
+        ft = { 'csv', 'tsv' },
+        desc = 'Disabled Tabular View',
+      },
+      {
+        '<localleader>t',
+        '<cmd>CsvViewToggle<cr>',
+        ft = { 'csv', 'tsv' },
+        desc = 'Toggle Tabular View',
+      },
+    },
+  },
 }
