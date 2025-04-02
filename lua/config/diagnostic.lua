3d6ff94e0ee4838189c979db1369224742e7e6cd
Michael van der Loo <michael.vanderloo@ll.mit.edu>
2025-04-02 13:55:04 -0400

    use native diagnostics

---
+++ lua/config/diagnostic.lua
@@ -0,0 +1,25 @@
+vim.diagnostic.config {
+  underline = true,
+  virtual_text = {
+    prefix = '',
+    severity = nil,
+    source = 'if_many',
+    format = nil,
+  },
+  severity_sort = true,
+  update_in_insert = false,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = 'E',
+      [vim.diagnostic.severity.WARN] = 'W',
+      [vim.diagnostic.severity.INFO] = 'I',
+      [vim.diagnostic.severity.HINT] = 'H',
+    },
+    -- linehl = {
+    --   [vim.diagnostic.severity.ERROR] = 'Error',
+    --   [vim.diagnostic.severity.WARN] = 'Warn',
+    --   [vim.diagnostic.severity.INFO] = 'Info',
+    --   [vim.diagnostic.severity.HINT] = 'Hint',
+    -- },
+  },
+}
