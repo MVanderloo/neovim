8b6ca0da6b2eda329f6b16abca372671547a62fb
Michael van der Loo <github@mvanderloo.com>
2025-04-06 23:30:40 +0100

    big changes coming

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -12,31 +12,57 @@ return {
       indent = { enable = true, disable = { 'ruby' } },
     },
   },
-  -- { 'nvim-treesitter/nvim-treesitter-context' }, -- this is a bit intrusive
   {
     'nvim-treesitter/nvim-treesitter-textobjects',
-    dependencies = 'nvim-treesitter/nvim-treesitter',
-    event = { 'BufReadPost', 'BufNewFile' },
-    config = function()
-      require('nvim-treesitter.configs').setup {
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    init = function()
+      local config = require 'nvim-treesitter.configs'
+      config.setup {
         textobjects = {
           select = {
             enable = true,
             lookahead = true,
             keymaps = {
-              ['aa'] = '@parameter.outer',
-              ['ia'] = '@parameter.inner',
               ['af'] = '@function.outer',
               ['if'] = '@function.inner',
+              -- ['ac'] = '@class.outer',
+              -- ['ic'] = '@class.inner',
+              ['ao'] = '@comment.outer',
+              -- ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
               ['aC'] = '@class.outer',
               ['iC'] = '@class.inner',
               ['ic'] = '@conditional.inner',
               ['ac'] = '@conditional.outer',
               ['il'] = '@loop.inner',
               ['al'] = '@loop.outer',
-              ['at'] = '@comment.outer',
             },
+            selection_modes = {
+              ['@parameter.outer'] = 'v',
+              ['@function.outer'] = 'V',
+              ['@class.outer'] = '<c-v>',
+            },
+            -- If you set this to `true` (default is `false`) then any textobject is
+            -- extended to include preceding or succeeding whitespace. Succeeding
+            -- whitespace has priority in order to act similarly to eg the built-in
+            -- `ap`.
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * selection_mode: eg 'v'
+            -- and should return true or false
+            include_surrounding_whitespace = true,
           },
+          -- swap = {
+          --   enable = true,
+          --   swap_next = {
+          --     ['<leader>a'] = { query = '@parameter.inner', desc = 'Swap with next parameter' },
+          --   },
+          --   swap_previous = {
+          --     ['<leader>A'] = '@parameter.inner',
+          --   },
+          -- },
           -- move = {
           --   enable = true,
           --   set_jumps = true, -- whether to set jumps in the jumplist
@@ -63,7 +89,7 @@ return {
   },
   {
     'folke/ts-comments.nvim',
-    opts = {},
+    config = true,
     event = 'VeryLazy',
   },
 }
