062343dca438771b337ff930ae9fac813a47d375
Michael van der Loo <github@mvanderloo.com>
2025-04-06 22:46:58 +0100

    source config after lazy loads plugins

--- init.lua
+++ init.lua
@@ -14,8 +14,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require 'config'
-
 require('lazy').setup {
   spec = { { import = 'plugins' } },
   defaults = { { lazy = true } },
@@ -36,3 +34,5 @@ require('lazy').setup {
     },
   },
 }
+
+require 'config'
