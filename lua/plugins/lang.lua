a666c51422d2874358ab5d5bc82281aca8bc6587
Michael Vanderloo <mvanderloo@Michaels-MacBook-Air.local>
2025-04-07 17:12:47 +0100

    move csv-view out of lang

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -97,41 +97,4 @@ return {
       }
     end,
   },
-  {
-    'hat0uma/csvview.nvim',
-    ft = { 'csv', 'tsv' },
-    opts = {
-      view = {
-        min_column_width = 1,
-        spacing = 0,
-        display_mode = 'border',
-      },
-      delimiter = {
-        default = ',',
-        ft = {
-          tsv = '\t',
-        },
-      },
-    },
-    keys = {
-      {
-        '<localleader>e',
-        '<cmd>CsvViewEnable<cr>',
-        ft = { 'csv', 'tsv' },
-        desc = 'Enable Tabular View',
-      },
-      {
-        '<localleader>d',
-        '<cmd>CsvViewDisable<cr>',
-        ft = { 'csv', 'tsv' },
-        desc = 'Disabled Tabular View',
-      },
-      {
-        '<localleader>t',
-        '<cmd>CsvViewToggle<cr>',
-        ft = { 'csv', 'tsv' },
-        desc = 'Toggle Tabular View',
-      },
-    },
-  },
 }
