9f1d0e7387ac7085ad07e2db16d976e198af06cf
Michael Vanderloo <mvanderloo@Michaels-MacBook-Air.local>
2025-04-07 17:13:30 +0100

    add more editor plugins

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,3 +1,8 @@
 return {
-	'nmac427/guess-indent.nvim'
+  'nmac427/guess-indent.nvim',
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl" },
+  { 'echasnovski/mini.ai', version = '*', lazy = false },
+  { 'echasnovski/mini.align', version = '*', lazy = false, config = true },
+  { 'echasnovski/mini.operators', version = '*', lazy = false, config = true },
+  { 'echasnovski/mini.surround', version = '*', lazy = false, config = true },
 }
