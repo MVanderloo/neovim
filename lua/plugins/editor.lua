51b97c485378d45a03639b213ee32ccbc46914fc
Michael van der Loo <michael.vanderloo@ll.mit.edu>
2025-04-12 21:15:26 -0400

    editor plugins

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,8 +1,9 @@
 return {
   'nmac427/guess-indent.nvim',
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl" },
-  { 'echasnovski/mini.ai', version = '*', lazy = false },
-  { 'echasnovski/mini.align', version = '*', lazy = false, config = true },
-  { 'echasnovski/mini.operators', version = '*', lazy = false, config = true },
-  { 'echasnovski/mini.surround', version = '*', lazy = false, config = true },
+  { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', event = 'InsertEnter' },
+  { 'echasnovski/mini.ai', version = '*', lazy = 'VeryLazy' },
+  -- { 'echasnovski/mini.align', version = '*', config = true },
+  { 'echasnovski/mini.operators', version = '*', event = 'VeryLazy', config = true },
+  { 'echasnovski/mini.surround', version = '*', event = 'VeryLazy', config = true },
+  { 'echasnovski/mini.pairs', version = '*', event = 'InsertEnter', config = true },
 }
