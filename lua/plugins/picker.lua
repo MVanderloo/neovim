4ccd9a7d01d09c71251a06e8080f3e8c15deb7ed
Michael van der Loo <github@mvanderloo.com>
2025-04-12 21:20:02 -0400

    restore LSP keybindings

--- lua/plugins/picker.lua
+++ lua/plugins/picker.lua
@@ -70,11 +70,24 @@ return {
         desc = '[/] Live grep the current buffer',
       },
     },
+    opts = {
+      "default-title",
+      fzf_colors = true,
+      fzf = {
+        -- ['ctrl-d'] = 'preview-page-down',
+        -- ['ctrl-u'] = 'preview-page-up',
+        -- ['ctrl-q'] = 'select-all+accept',
+      },
+      actions = {
+        -- files = {
+        --   ['default'] = require("fzf-lua").actions.file_edit_or_qf,
+        -- },
+      },
+    },
   },
   {
     'folke/snacks.nvim',
     opts = {
-      explorer = { enabled = true, replace_netrw = true },
       picker = { enabled = true },
     },
     keys = {
@@ -83,7 +96,6 @@ return {
       -- { '<leader>/', function() Snacks.picker.grep() end, desc = 'Grep' },
       -- { '<leader>:', function() Snacks.picker.command_history() end, desc = 'Command History' },
       -- { '<leader>n', function() Snacks.picker.notifications() end, desc = 'Notification History' },
-      -- { '<leader>e', function() Snacks.explorer() end, desc = 'File Explorer' },
       -- -- find
       -- { '<leader>fb', function() Snacks.picker.buffers() end, desc = 'Buffers' },
       -- { '<leader>fc', function() Snacks.picker.files { cwd = vim.fn.stdpath 'config' } end, desc = 'Find Config File' },
@@ -127,11 +139,11 @@ return {
       -- { '<leader>su', function() Snacks.picker.undo() end, desc = 'Undo History' },
       -- { '<leader>uC', function() Snacks.picker.colorschemes() end, desc = 'Colorschemes' },
       -- -- LSP
-      -- { 'gd', function() Snacks.picker.lsp_definitions() end, desc = 'Goto Definition' },
-      -- { 'gD', function() Snacks.picker.lsp_declarations() end, desc = 'Goto Declaration' },
-      -- { 'gr', function() Snacks.picker.lsp_references() end, nowait = true, desc = 'References' },
-      -- { 'gI', function() Snacks.picker.lsp_implementations() end, desc = 'Goto Implementation' },
-      -- { 'gy', function() Snacks.picker.lsp_type_definitions() end, desc = 'Goto T[y]pe Definition' },
+      { 'gd', function() Snacks.picker.lsp_definitions() end, desc = 'Goto Definition' },
+      { 'gD', function() Snacks.picker.lsp_declarations() end, desc = 'Goto Declaration' },
+      { 'gr', function() Snacks.picker.lsp_references() end, nowait = true, desc = 'References' },
+      { 'gI', function() Snacks.picker.lsp_implementations() end, desc = 'Goto Implementation' },
+      { 'gy', function() Snacks.picker.lsp_type_definitions() end, desc = 'Goto T[y]pe Definition' },
       -- { '<leader>ls', function() Snacks.picker.lsp_symbols() end, desc = 'LSP Symbols' },
       -- { '<leader>lw', function() Snacks.picker.lsp_workspace_symbols() end, desc = 'LSP Workspace Symbols' },
     },
