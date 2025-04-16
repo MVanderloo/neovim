73a0d0c59c0f96725a009c4346788cf5ff177329
Michael van der Loo <github@mvanderloo.com>
2025-04-16 11:01:07 -0400

    cleaner

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,21 +1,19 @@
 return {
-  {
-    'christoomey/vim-tmux-navigator',
-    event = 'VeryLazy',
-    cmd = {
-      'TmuxNavigateLeft',
-      'TmuxNavigateDown',
-      'TmuxNavigateUp',
-      'TmuxNavigateRight',
-      'TmuxNavigatePrevious',
-      'TmuxNavigatorProcessList',
-    },
-    keys = {
-      { '<C-h>', '<cmd>TmuxNavigateLeft("n")<cr>', mode = { 'n', 'v', 'i' }, desc = 'TmuxNavigateLeft' },
-      { '<C-j>', '<cmd>TmuxNavigateDown<cr>', mode = { 'n', 'v', 'i' }, desc = 'TmuxNavigateDown' },
-      { '<C-k>', '<cmd>TmuxNavigateUp<cr>', mode = { 'n', 'v', 'i' }, desc = 'TmuxNavigateUp' },
-      { '<C-l>', '<cmd>TmuxNavigateRight<cr>', mode = { 'n', 'v', 'i' }, desc = 'TmuxNavigateRight' },
-      { '<C-\\>', '<cmd>TmuxNavigatePrevious<cr>', mode = { 'n', 'v', 'i' }, desc = 'TmuxNavigatePrevious' },
-    },
+  'christoomey/vim-tmux-navigator',
+  event = 'VeryLazy',
+  cmd = {
+    'TmuxNavigateLeft',
+    'TmuxNavigateDown',
+    'TmuxNavigateUp',
+    'TmuxNavigateRight',
+    'TmuxNavigatePrevious',
+    'TmuxNavigatorProcessList',
+  },
+  keys = {
+    { '<C-h>', '<cmd>TmuxNavigateLeft("n")<cr>', mode = { 'n', 'v', 'i' }, desc = 'TmuxNavigateLeft' },
+    { '<C-j>', '<cmd>TmuxNavigateDown<cr>', mode = { 'n', 'v', 'i' }, desc = 'TmuxNavigateDown' },
+    { '<C-k>', '<cmd>TmuxNavigateUp<cr>', mode = { 'n', 'v', 'i' }, desc = 'TmuxNavigateUp' },
+    { '<C-l>', '<cmd>TmuxNavigateRight<cr>', mode = { 'n', 'v', 'i' }, desc = 'TmuxNavigateRight' },
+    { '<C-\\>', '<cmd>TmuxNavigatePrevious<cr>', mode = { 'n', 'v', 'i' }, desc = 'TmuxNavigatePrevious' },
   },
 }
