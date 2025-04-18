091ab49665c0748515399e1aa13078dbe90159dd
Michael van der Loo <michael.vanderloo@ll.mit.edu>
2025-04-18 12:15:16 -0400

    expanding lsp attach autocommand

--- lua/config/auto-commands.lua
+++ lua/config/auto-commands.lua
@@ -38,16 +38,25 @@ vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
 -- start terminal in insert mode
 vim.api.nvim_create_autocmd('TermOpen', { command = 'startinsert | set winfixheight' })
 
--- setup LSP folding on attach, if supported
+-- integrate LSP capabilities on attach
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
 
+    if client == nil then
+      return
+    end
+
+    -- folding
     if client:supports_method 'textDocument/foldingRange' then
       local win = vim.api.nvim_get_current_win()
       vim.wo[win][0].foldmethod = 'expr'
       vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
     end
+
+    -- keybindings
+  --   if client:supports_method then
+  --     vim.keymap.set('i', '<leader>la')
   end,
 })
 
