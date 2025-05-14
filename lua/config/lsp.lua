3df0e37b1e316d3cde9f7c5854823dd44aff4e3a
Michael van der Loo <github@mvanderloo.com>
2025-05-14 11:37:01 -0400

    clean up unused diagnostic config

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,10 +1,3 @@
--- local diagnostic_icons = {
---   ERROR = '',
---   WARN = '',
---   HINT = '',
---   INFO = '',
--- }
-
 local methods = vim.lsp.protocol.Methods
 
 local M = {}
@@ -100,58 +93,6 @@ local function on_attach(client, bufnr)
     })
   end
 end
-
--- Define the diagnostic signs.
--- for severity, icon in pairs(diagnostic_icons) do
---   local hl = 'DiagnosticSign' .. severity:sub(1, 1) .. severity:sub(2):lower()
---   vim.fn.sign_define(hl, { text = icon, texthl = hl })
--- end
-
--- Diagnostic configuration.
--- vim.diagnostic.config {
---   virtual_text = {
---     prefix = '',
---     spacing = 2,
---     format = function(diagnostic)
---       -- Use shorter, nicer names for some sources:
---       local special_sources = {
---         ['Lua Diagnostics.'] = 'lua',
---         ['Lua Syntax Check.'] = 'lua',
---       }
---
---       local message = diagnostic_icons[vim.diagnostic.severity[diagnostic.severity]]
---       if diagnostic.source then
---         message = string.format('%s %s', message, special_sources[diagnostic.source] or diagnostic.source)
---       end
---       if diagnostic.code then message = string.format('%s[%s]', message, diagnostic.code) end
---
---       return message .. ' '
---     end,
---   },
---   float = {
---     source = 'if_many',
---     -- Show severity icons as prefixes.
---     prefix = function(diag)
---       local level = vim.diagnostic.severity[diag.severity]
---       local prefix = string.format(' %s ', diagnostic_icons[level])
---       return prefix, 'Diagnostic' .. level:gsub('^%l', string.upper)
---     end,
---   },
---   -- Disable signs in the gutter.
---   signs = false,
--- }
-
--- Override the virtual text diagnostic handler so that the most severe diagnostic is shown first.
--- local show_handler = vim.diagnostic.handlers.virtual_text.show
--- assert(show_handler)
--- local hide_handler = vim.diagnostic.handlers.virtual_text.hide
--- vim.diagnostic.handlers.virtual_text = {
---   show = function(ns, bufnr, diagnostics, opts)
---     table.sort(diagnostics, function(diag1, diag2) return diag1.severity > diag2.severity end)
---     return show_handler(ns, bufnr, diagnostics, opts)
---   end,
---   hide = hide_handler,
--- }
 
 ---@diagnostic disable-next-line: duplicate-set-field
 -- vim.lsp.buf.hover = function()
