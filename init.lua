072c4b002e7828208461b44392e8b88b576565a1
Michael van der Loo <github@mvanderloo.com>
2025-05-31 15:39:18 -0400

    run this block in 0.12 and later

--- init.lua
+++ init.lua
@@ -19,8 +19,10 @@ vim.g.maplocalleader = '\\'
 
 -- experimental UI for 0.12
 -- https://github.com/neovim/neovim/pull/27855
--- require('vim._extui').enable {}
--- vim.opt.cmdheight = 0
+if vim.inspect(vim.version().minor >= 12) then
+  require('vim._extui').enable {}
+  vim.opt.cmdheight = 0
+end
 
 require('lazy').setup {
   spec = { import = 'plugins' },
