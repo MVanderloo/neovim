bec3e77114b14dbdd6512801c01c21a3c258b1ac
Michael van der Loo <michaeljvanderloo@gmail.com>
2025-06-20 00:04:13 -0400

    pretty markdown and help files with keybindings

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,11 +1,19 @@
 return {
+  -- {
+  --   'MeanderingProgrammer/render-markdown.nvim',
+  --   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
+  --   ftype = { 'markdown' },
+  --   opts = {
+  --     -- completions was causing some error while i was not in markdown
+  --     -- completions = { blink = { enabled = true } }
+  --   },
+  -- },
   {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
-    ftype = { 'markdown' },
-    opts = {
-      -- completions was causing some error while i was not in markdown
-      -- completions = { blink = { enabled = true } }
-    },
+    'OXY2DEV/markview.nvim',
+    lazy = false,
+  },
+  {
+    'OXY2DEV/helpview.nvim',
+    lazy = false,
   },
 }
