19340a500b245bfd60db36e9c37b23f6bb891036
Michael van der Loo <github@mvanderloo.com>
2025-06-20 00:04:13 -0400

    pretty markdown and help files with keybindings

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,7 @@
+vim.keymap.set('n', '<localleader>E', '<cmd>Markview Enable<cr>')
+vim.keymap.set('n', '<localleader>D', '<cmd>Markview Disable<cr>')
+vim.keymap.set('n', '<localleader>T', '<cmd>Markview Toggle<cr>')
+
+vim.keymap.set('n', '<localleader>e', '<cmd>Markview enable<cr>')
+vim.keymap.set('n', '<localleader>d', '<cmd>Markview disable<cr>')
+vim.keymap.set('n', '<localleader>t', '<cmd>Markview toggle<cr>')
