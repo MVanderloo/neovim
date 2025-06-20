a2b48f5efece4d0b12ce08bc8f4932506f820285
Michael van der Loo <github@mvanderloo.com>
2025-06-20 00:04:13 -0400

    pretty markdown and help files with keybindings

--- ftplugin/help.lua
+++ ftplugin/help.lua
@@ -1,2 +1,10 @@
 vim.cmd 'normal! M'
 vim.opt_local.scrolloff = 999
+
+vim.keymap.set('n', '<localleader>E', '<cmd>Helpview Enable<cr>')
+vim.keymap.set('n', '<localleader>D', '<cmd>Helpview Disable<cr>')
+vim.keymap.set('n', '<localleader>T', '<cmd>Helpview Toggle<cr>')
+
+vim.keymap.set('n', '<localleader>e', '<cmd>Helpview enable<cr>')
+vim.keymap.set('n', '<localleader>d', '<cmd>Helpview disable<cr>')
+vim.keymap.set('n', '<localleader>t', '<cmd>Helpview toggle<cr>')
