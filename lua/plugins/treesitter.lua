ce3e701837282d0ff81779483d933d44775c410e
Michael van der Loo <github@mvanderloo.com>
2025-06-23 22:14:40 -0400

    update treesitter to main branch, disable treesitter plugins for now

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -13,10 +13,11 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    branch = 'main',
     build = ':TSUpdate',
     opts = {
       ensure_installed = 'all',
-      auto_install = true,
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = { 'ruby' },
@@ -24,94 +25,94 @@ return {
       indent = { enable = true, disable = { 'ruby' } },
     },
   },
-  {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    event = { 'VeryLazy' },
-    init = function()
-      local config = require 'nvim-treesitter.configs'
-      config.setup {
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['a/'] = '@comment.outer',
-              ['i/'] = '@comment.inner',
-              -- ['aa'] = '@parameter.outer',
-              -- ['ia'] = '@parameter.inner',
-              ['aC'] = '@class.outer',
-              ['iC'] = '@class.inner',
-              ['ac'] = '@conditional.outer',
-              ['ic'] = '@conditional.inner',
-              ['al'] = '@loop.outer',
-              ['il'] = '@loop.inner',
-              ['aB'] = '@block.outer',
-              ['iB'] = '@block.inner',
-            },
-            selection_modes = {
-              ['@function.outer'] = 'v',
-              ['@function.inner'] = 'v',
-              ['@comment.outer'] = 'v',
-              ['@comment.inner'] = 'v',
-              ['@parameter.outer'] = 'v',
-              ['@parameter.inner'] = 'v',
-              ['@class.outer'] = 'V',
-              ['@class.inner'] = 'V',
-              ['@conditional.outer'] = 'v',
-              ['@conditional.inner'] = 'v',
-              ['@loop.outer'] = 'v',
-              ['@loop.inner'] = 'v',
-              ['@block.outer'] = 'V',
-              ['@block.inner'] = 'V',
-            },
-            include_surrounding_whitespace = true,
-          },
-          -- move = {
-          --   enable = true,
-          --   set_jumps = true,
-          --   goto_next_start = {
-          --     [']f'] = '@function.outer',
-          --     [']/'] = '@comment.outer',
-          --     [']a'] = '@parameter.outer',
-          --     [']C'] = '@class.outer',
-          --     [']c'] = '@conditional.outer',
-          --     [']B'] = '@block.outer',
-          --   },
-          --   goto_previous_start = {
-          --     ['[f'] = '@function.outer',
-          --     ['[/'] = '@comment.outer',
-          --     ['[a'] = '@parameter.outer',
-          --     ['[C'] = '@class.outer',
-          --     ['[c'] = '@conditional.outer',
-          --     ['[B'] = '@block.outer',
-          --   },
-          -- },
-          -- swap = {
-          --   enable = true,
-          --   swap_next = {
-          --     ['snf'] = '@function.outer',
-          --     ['sna'] = '@parameter.inner',
-          --     ['snC'] = '@parameter.inner',
-          --     ['snc'] = '@parameter.inner',
-          --     ['snl'] = '@parameter.inner',
-          --     ['snB'] = '@parameter.inner',
-          --   },
-          --   swap_previous = {
-          --     ['spf'] = '@parameter.inner',
-          --     ['spa'] = '@parameter.inner',
-          --     ['spC'] = '@parameter.inner',
-          --     ['spc'] = '@parameter.inner',
-          --     ['spl'] = '@parameter.inner',
-          --     ['spB'] = '@parameter.inner',
-          --   }
-          -- },
-        },
-      }
-    end,
-  },
+  -- {
+  --   'nvim-treesitter/nvim-treesitter-textobjects',
+  --   dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  --   event = { 'VeryLazy' },
+  --   init = function()
+  --     local config = require 'nvim-treesitter.configs'
+  --     config.setup {
+  --       textobjects = {
+  --         -- select = {
+  --         --   enable = true,
+  --         --   lookahead = true,
+  --         --   keymaps = {
+  --         --     ['af'] = '@function.outer',
+  --         --     ['if'] = '@function.inner',
+  --         --     ['a/'] = '@comment.outer',
+  --         --     ['i/'] = '@comment.inner',
+  --         --     ['aa'] = '@parameter.outer',
+  --         --     ['ia'] = '@parameter.inner',
+  --         --     ['aC'] = '@class.outer',
+  --         --     ['iC'] = '@class.inner',
+  --         --     ['ac'] = '@conditional.outer',
+  --         --     ['ic'] = '@conditional.inner',
+  --         --     ['al'] = '@loop.outer',
+  --         --     ['il'] = '@loop.inner',
+  --         --     ['aB'] = '@block.outer',
+  --         --     ['iB'] = '@block.inner',
+  --         --   },
+  --         --   selection_modes = {
+  --         --     ['@function.outer'] = 'v',
+  --         --     ['@function.inner'] = 'v',
+  --         --     ['@comment.outer'] = 'v',
+  --         --     ['@comment.inner'] = 'v',
+  --         --     ['@parameter.outer'] = 'v',
+  --         --     ['@parameter.inner'] = 'v',
+  --         --     ['@class.outer'] = 'V',
+  --         --     ['@class.inner'] = 'V',
+  --         --     ['@conditional.outer'] = 'v',
+  --         --     ['@conditional.inner'] = 'v',
+  --         --     ['@loop.outer'] = 'v',
+  --         --     ['@loop.inner'] = 'v',
+  --         --     ['@block.outer'] = 'V',
+  --         --     ['@block.inner'] = 'V',
+  --         --   },
+  --         --   include_surrounding_whitespace = true,
+  --         -- },
+  --         -- move = {
+  --         --   enable = true,
+  --         --   set_jumps = true,
+  --         --   goto_next_start = {
+  --         --     [']f'] = '@function.outer',
+  --         --     [']/'] = '@comment.outer',
+  --         --     [']a'] = '@parameter.outer',
+  --         --     [']C'] = '@class.outer',
+  --         --     [']c'] = '@conditional.outer',
+  --         --     [']B'] = '@block.outer',
+  --         --   },
+  --         --   goto_previous_start = {
+  --         --     ['[f'] = '@function.outer',
+  --         --     ['[/'] = '@comment.outer',
+  --         --     ['[a'] = '@parameter.outer',
+  --         --     ['[C'] = '@class.outer',
+  --         --     ['[c'] = '@conditional.outer',
+  --         --     ['[B'] = '@block.outer',
+  --         --   },
+  --         -- },
+  --         -- swap = {
+  --         --   enable = true,
+  --         --   swap_next = {
+  --         --     ['snf'] = '@function.outer',
+  --         --     ['sna'] = '@parameter.inner',
+  --         --     ['snC'] = '@parameter.inner',
+  --         --     ['snc'] = '@parameter.inner',
+  --         --     ['snl'] = '@parameter.inner',
+  --         --     ['snB'] = '@parameter.inner',
+  --         --   },
+  --         --   swap_previous = {
+  --         --     ['spf'] = '@parameter.inner',
+  --         --     ['spa'] = '@parameter.inner',
+  --         --     ['spC'] = '@parameter.inner',
+  --         --     ['spc'] = '@parameter.inner',
+  --         --     ['spl'] = '@parameter.inner',
+  --         --     ['spB'] = '@parameter.inner',
+  --         --   }
+  --         -- },
+  --       },
+  --     }
+  --   end,
+  -- },
   { 'folke/ts-comments.nvim', config = true, event = 'VeryLazy' },
   {
     'Wansmer/treesj',
