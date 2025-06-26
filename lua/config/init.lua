ecd3159512b158efd989b329f3f07586e46f37eb
Michael van der Loo <github@mvanderloo.com>
2025-06-26 10:58:26 -0400

    format

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -4,4 +4,4 @@ require 'config.auto-commands'
 require 'config.filetype'
 require 'config.lsp'
 
-vim.api.nvim_set_hl(0, 'WinSeparator', { fg = 'white'})
+vim.api.nvim_set_hl(0, 'WinSeparator', { fg = 'white' })
