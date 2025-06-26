6691034979182e3c86a6a7282ee94ea27b091beb
Michael van der Loo <github@mvanderloo.com>
2025-06-26 11:05:58 -0400

    remove rtp message markview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -12,6 +12,7 @@ return {
     'OXY2DEV/markview.nvim',
     lazy = false,
     dependencies = { 'saghen/blink.cmp' },
+    opts = { experimental = { check_rtp_message = false } },
   },
   {
     'OXY2DEV/helpview.nvim',
