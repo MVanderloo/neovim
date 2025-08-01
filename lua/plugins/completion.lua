c00c37abd398b2edba7192884661cc0253d2c074
Michael van der Loo <michael.vanderloo@ll.mit.edu>
2025-08-01 10:46:17 -0400

    update blink-ripgrep config

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -84,12 +84,14 @@ return {
             score_offset = 10,
             opts = {
               prefix_min_len = 3,
-              context_size = 5,
-              max_filesize = '1M',
-              project_root_marker = '.git',
-              project_root_fallback = true,
-              search_casing = '--ignore-case', -- '--smart-case'
-              additional_paths = {},
+              backend = {
+                context_size = 5,
+                max_filesize = '1M',
+                project_root_marker = '.git',
+                project_root_fallback = true,
+                search_casing = '--ignore-case', -- '--smart-case'
+                additional_paths = {},
+              },
             },
           },
 
