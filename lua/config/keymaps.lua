774271b596da2e487a52c249c96e5b3e76fb2f90
Michael van der Loo <github@mvanderloo.com>
2025-08-07 10:50:19 -0400

    clean up keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,14 +14,13 @@ set({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, 
 -- set({ 'n', 'x' }, '<Up>', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
 
 -- Have Esc clear highlighted search
-set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+set('n', '<Esc>', '<cmd>nohlsearch<cr>')
 
 -- toggle folds with tab
 set('n', '<Tab>', 'za', { silent = true, noremap = true })
--- Ctrl-i is the same thing as tab - use ctrl-shift-o to jump forwards
+--
+-- Ctrl-i is the same thing as tab - use ctrl-shift-o to jump forward
 set('n', '<C-S-o>', '<C-i>', { silent = true, noremap = true })
--- alternatively mini.bracketed binds [j and ]j but it's less spammable
--- if i can get used to using enter to toggle tab, I won't need the above workaround
 
 -- Keep highlight when indenting block
 set('v', '<', '<gv', { silent = true, noremap = true })
@@ -44,19 +43,14 @@ set('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window W
 set({ 'n', 'x', 'o' }, 'n', "v:searchforward ? 'nzz' : 'Nzz'", { expr = true, desc = 'Next Search Result' })
 set({ 'n', 'x', 'o' }, 'N', "v:searchforward ? 'Nzz' : 'nzz'", { expr = true, desc = 'Prev Search Result' })
 
--- Lazy.nvim
-set({ 'n', 'x' }, '<leader>L', '<CMD>Lazy<CR>', { desc = 'Open Lazy' })
-
 -- TODO activate these on LSP attach
-set('n', '<leader>la', function() vim.lsp.buf.code_action { apply = true } end, { desc = 'Code actions' })
-set('n', '<leader>lr', function() vim.lsp.buf.rename() end, { desc = 'Rename' })
+set('n', 'gla', function() vim.lsp.buf.code_action { apply = true } end, { desc = 'Code actions' })
+set('n', 'glr', function() vim.lsp.buf.rename() end, { desc = 'Rename' })
 
-set('n', '<leader>th', function()
-  local enabled = vim.lsp.inlay_hint.is_enabled { bufnr = 0 }
-  vim.lsp.inlay_hint.enable(not enabled, { bufnr = 0 })
-end, { desc = 'Toggle LSP inlay hints' })
-
-set('n', '-', '<cmd>Oil<cr>', { desc = 'Open Parent Directory' })
+-- set('n', '<leader>th', function()
+--   local enabled = vim.lsp.inlay_hint.is_enabled { bufnr = 0 }
+--   vim.lsp.inlay_hint.enable(not enabled, { bufnr = 0 })
+-- end, { desc = 'Toggle LSP inlay hints' })
 
 -- set by mini.bracketed
 -- set({ 'n', 'x' }, ']d', function()
@@ -69,13 +63,9 @@ set('n', '-', '<cmd>Oil<cr>', { desc = 'Open Parent Directory' })
 --   if d ~= nil then vim.diagnostic.jump { diagnostic = d } end
 -- end)
 
-set({ 'n', 'x' }, '<leader>td', function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end)
+-- set({ 'n', 'x' }, '<leader>td', function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end)
 
 set('n', '<C-q>', vim.diagnostic.setqflist)
-
-set({ 'n', 'x' }, '<leader>sw', '<cmd>SessionSearch<CR>', { desc = 'Session search' })
-set({ 'n', 'x' }, '<leader>ss', '<cmd>SessionSave<CR>', { desc = 'Save session' })
-set({ 'n', 'x' }, '<leader>sa', '<cmd>SessionToggleAutoSave<CR>', { desc = 'Toggle autosave' })
 
 -- tab navigation
 set({ 'n', 'x' }, '[[', 'gT', { desc = 'Previous Tab' })
