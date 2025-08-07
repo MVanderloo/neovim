f8906d85ef669475c9523490b0fb0612eba2daa9
Michael van der Loo <github@mvanderloo.com>
2025-08-06 23:30:39 -0400

    get some plugins working

--- lua/plugins/picker.lua
+++ lua/plugins/picker.lua
@@ -1,169 +1,121 @@
-return {
-  {
-    'ibhagwan/fzf-lua',
-    event = 'VeryLazy',
-    cmd = 'FzfLua',
-    init = function() require('fzf-lua').register_ui_select() end,
-    keys = {
-      { '<leader>f.', '<cmd>FzfLua resume<cr>', desc = 'Resume last command' },
-      {
-        '<leader>/',
-        function()
-          require('fzf-lua').lgrep_curbuf {
-            winopts = {
-              height = 0.6,
-              width = 0.5,
-              preview = { vertical = 'up:70%' },
-            },
-          }
-        end,
-        desc = 'Grep current buffer',
+vim.pack.add {
+  'gh:ibhagwan/fzf-lua',
+}
+
+local fzf = require 'fzf-lua'
+fzf.register_ui_select()
+
+local keymap = vim.keymap.set
+keymap('n', '<leader>f.', '<cmd>FzfLua resume<cr>', { desc = 'Resume last command' })
+keymap(
+  'n',
+  '<leader>/',
+  function()
+    require('fzf-lua').lgrep_curbuf {
+      winopts = {
+        height = 0.6,
+        width = 0.5,
+        preview = { vertical = 'up:70%' },
       },
-      { '<leader>fc', '<cmd>FzfLua highlights<cr>', desc = 'Highlights' },
-      { '<leader>fd', '<cmd>FzfLua lsp_document_diagnostics<cr>', desc = 'Document diagnostics' },
-      { '<leader>fD', '<cmd>FzfLua lsp_workspace_diagnostics<cr>', desc = 'Workspace diagnostics' },
-      { '<leader>ff', '<cmd>FzfLua files<cr>', desc = 'Find files' },
-      { '<leader>fg', '<cmd>FzfLua live_grep<cr>', desc = 'Grep' },
-      { '<leader>fg', '<cmd>FzfLua grep_visual<cr>', desc = 'Grep', mode = 'x' },
-      { '<leader>fh', '<cmd>FzfLua help_tags<cr>', desc = 'Help' },
-      {
-        '<leader>fr',
-        function()
-          -- Read from ShaDa to include files that were already deleted from the buffer list.
-          vim.cmd 'rshada!'
-          require('fzf-lua').oldfiles()
-        end,
-        desc = 'Recently opened files',
-      },
-      { 'z=', '<cmd>FzfLua spell_suggest<cr>', desc = 'Spelling suggestions' },
+    }
+  end,
+  { desc = 'Grep current buffer' }
+)
+keymap('n', '<leader>fc', '<cmd>FzfLua highlights<cr>', { desc = 'Highlights' })
+keymap('n', '<leader>fd', '<cmd>FzfLua lsp_document_diagnostics<cr>', { desc = 'Document diagnostics' })
+keymap('n', '<leader>fD', '<cmd>FzfLua lsp_workspace_diagnostics<cr>', { desc = 'Workspace diagnostics' })
+keymap('n', '<leader>ff', '<cmd>FzfLua files<cr>', { desc = 'Find files' })
+keymap('n', '<leader>fg', '<cmd>FzfLua live_grep<cr>', { desc = 'Grep' })
+keymap('x', '<leader>fg', '<cmd>FzfLua grep_visual<cr>', { desc = 'Grep' })
+keymap('n', '<leader>fh', '<cmd>FzfLua help_tags<cr>', { desc = 'Help' })
+keymap('n', '<leader>fr', function()
+  vim.cmd 'rshada!'
+  require('fzf-lua').oldfiles()
+end, { desc = 'Recently opened files' })
+keymap('n', 'z=', '<cmd>FzfLua spell_suggest<cr>', { desc = 'Spelling suggestions' })
+
+require('fzf-lua').setup {
+  fzf_colors = true,
+  fzf_opts = {
+    ['--info'] = 'default',
+    ['--layout'] = 'reverse-list',
+  },
+  keymap = {
+    builtin = {
+      ['<C-/>'] = 'toggle-help',
+      ['<C-a>'] = 'select-all',
+      ['<C-i>'] = 'toggle+down',
+      ['<C-S-i>'] = 'toggle+up',
+      ['<C-f>'] = 'preview-page-down',
+      ['<C-b>'] = 'preview-page-up',
     },
-    opts = function()
-      local actions = require('fzf-lua').actions
-      return {
-        -- { 'telescope' },
-        fzf_colors = {
-          -- true,
-          -- bg = { 'bg', 'Normal' },
-          -- gutter = { 'bg', 'Normal' },
-          -- info = { 'fg', 'Conditional' },
-          -- scrollbar = { 'bg', 'Normal' },
-          -- separator = { 'fg', 'Comment' },
-        },
-        fzf_opts = {
-          ['--info'] = 'default',
-          ['--layout'] = 'reverse-list',
-        },
-        keymap = {
-          builtin = {
-            ['<C-/>'] = 'toggle-help',
-            ['<C-a>'] = 'select-all',
-            ['<C-i>'] = 'toggle+down',
-            ['<C-S-i>'] = 'toggle+up',
-            ['<C-f>'] = 'preview-page-down',
-            ['<C-b>'] = 'preview-page-up',
-            -- -- neovim `:tmap` mappings for the fzf win
-            -- -- true,        -- uncomment to inherit all the below in your custom config
-            -- ["<M-Esc>"]     = "hide",     -- hide fzf-lua, `:FzfLua resume` to continue
-            -- ["<F1>"]        = "toggle-help",
-            -- ["<F2>"]        = "toggle-fullscreen",
-            -- -- Only valid with the 'builtin' previewer
-            -- ["<F3>"]        = "toggle-preview-wrap",
-            -- ["<F4>"]        = "toggle-preview",
-            -- -- Rotate preview clockwise/counter-clockwise
-            -- ["<F5>"]        = "toggle-preview-ccw",
-            -- ["<F6>"]        = "toggle-preview-cw",
-            -- -- `ts-ctx` binds require `nvim-treesitter-context`
-            -- ["<F7>"]        = "toggle-preview-ts-ctx",
-            -- ["<F8>"]        = "preview-ts-ctx-dec",
-            -- ["<F9>"]        = "preview-ts-ctx-inc",
-            -- ["<S-Left>"]    = "preview-reset",
-            -- ["<S-down>"]    = "preview-page-down",
-            -- ["<S-up>"]      = "preview-page-up",
-            -- ["<M-S-down>"]  = "preview-down",
-            -- ["<M-S-up>"]    = "preview-up",
-          },
-          fzf = {
-            ['ctrl-z'] = 'abort',
-            ['ctrl-u'] = 'unix-line-discard',
-            ['ctrl-f'] = 'half-page-down',
-            ['ctrl-b'] = 'half-page-up',
-            -- ["ctrl-a"]      = "beginning-of-line",
-            -- ["ctrl-e"]      = "end-of-line",
-            ['ctrl-a'] = 'toggle-all',
-            ['alt-g'] = 'first',
-            ['alt-G'] = 'last',
-            -- ["f3"]          = "toggle-preview-wrap",
-            -- ["f4"]          = "toggle-preview",
-            ['shift-down'] = 'preview-page-down',
-            ['shift-up'] = 'preview-page-up',
-          },
-        },
-        actions = {
-          files = {
-            -- true,
-            ['ctrl-q'] = actions.file_edit_or_qf,
-            ['enter'] = actions.file_edit,
-            ['ctrl-s'] = actions.file_split,
-            ['ctrl-v'] = actions.file_vsplit,
-            ['ctrl-t'] = actions.file_tabedit,
-            ['alt-q'] = actions.file_sel_to_qf,
-            ['alt-Q'] = actions.file_sel_to_ll,
-            ['alt-i'] = actions.toggle_ignore,
-            ['alt-h'] = actions.toggle_hidden,
-            ['alt-f'] = actions.toggle_follow,
-          },
-        },
-        winopts = {
-          height = 0.8,
-          width = 0.7,
-          preview = {
-            scrollbar = false,
-            layout = 'vertical',
-            vertical = 'up:40%',
-          },
-        },
-        defaults = { git_icons = false },
-        previewers = {
-          codeaction = { toggle_behavior = 'extend' },
-        },
-        -- Configuration for specific commands.
-        -- files = {
-        --   winopts = {
-        --     preview = { hidden = true },
-        --   },
-        -- },
-        grep = {
-          rg_glob_fn = function(query, opts)
-            local regex, flags = query:match(string.format('^(.*)%s(.*)$', opts.glob_separator))
-            -- Return the original query if there's no separator.
-            return (regex or query), flags
-          end,
-        },
-        helptags = {
-          actions = {
-            ['enter'] = actions.help_vert,
-          },
-        },
-        lsp = {
-          code_actions = {
-            winopts = {
-              width = 70,
-              height = 20,
-              relative = 'cursor',
-              preview = {
-                hidden = true,
-                vertical = 'down:50%',
-              },
-            },
-          },
-        },
-        oldfiles = {
-          include_current_session = true,
-          winopts = {
-            preview = { hidden = true },
-          },
-        },
-      }
+    fzf = {
+      ['ctrl-z'] = 'abort',
+      ['ctrl-u'] = 'unix-line-discard',
+      ['ctrl-f'] = 'half-page-down',
+      ['ctrl-b'] = 'half-page-up',
+      ['ctrl-a'] = 'toggle-all',
+      ['alt-g'] = 'first',
+      ['alt-G'] = 'last',
+      ['shift-down'] = 'preview-page-down',
+      ['shift-up'] = 'preview-page-up',
+    },
+  },
+  actions = {
+    files = {
+      ['ctrl-q'] = fzf.actions.file_edit_or_qf,
+      ['enter'] = fzf.actions.file_edit,
+      ['ctrl-s'] = fzf.actions.file_split,
+      ['ctrl-v'] = fzf.actions.file_vsplit,
+      ['ctrl-t'] = fzf.actions.file_tabedit,
+      ['alt-q'] = fzf.actions.file_sel_to_qf,
+      ['alt-Q'] = fzf.actions.file_sel_to_ll,
+      ['alt-i'] = fzf.actions.toggle_ignore,
+      ['alt-h'] = fzf.actions.toggle_hidden,
+      ['alt-f'] = fzf.actions.toggle_follow,
+    },
+  },
+  winopts = {
+    height = 0.8,
+    width = 0.7,
+    preview = {
+      scrollbar = false,
+      layout = 'vertical',
+      vertical = 'up:40%',
+    },
+  },
+  defaults = { git_icons = false },
+  previewers = {
+    codeaction = { toggle_behavior = 'extend' },
+  },
+  grep = {
+    rg_glob_fn = function(query, opts)
+      local regex, flags = query:match(string.format('^(.*)%s(.*)$', opts.glob_separator))
+      return (regex or query), flags
     end,
+  },
+  helptags = {
+    actions = {
+      ['enter'] = fzf.actions.help_vert,
+    },
+  },
+  lsp = {
+    code_actions = {
+      winopts = {
+        width = 70,
+        height = 20,
+        relative = 'cursor',
+        preview = {
+          hidden = true,
+          vertical = 'down:50%',
+        },
+      },
+    },
+  },
+  oldfiles = {
+    include_current_session = true,
+    winopts = {
+      preview = { hidden = true },
+    },
   },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,110 +1,113 @@
-return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = { 'OXY2DEV/markview.nvim' }, -- markview provides treesitter queries and must be loaded first
-    branch = 'main',
-    build = ':TSUpdate',
-    opts = {
-      ensure_installed = 'all',
-      highlight = { enable = true },
-      indent = { enable = true },
-    },
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    branch = 'main',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    lazy = false, -- this may not be able to be lazy loaded
-    -- event = { 'VeryLazy' },
-    opts = {
-      select = {
-        lookahead = true,
-        selection_modes = {
-          ['@parameter.outer'] = 'v', -- charwise
-          ['@function.outer'] = 'V', -- linewise
-          ['@class.outer'] = '<c-v>', -- blockwise
-        },
-        include_surrounding_whitespace = false,
-      },
-    },
-    config = function()
-      vim.keymap.set(
-        { 'x', 'o' },
-        'af',
-        function() require('nvim-treesitter-textobjects.select').select_textobject('@function.outer', 'textobjects') end
-      )
-      vim.keymap.set(
-        { 'x', 'o' },
-        'if',
-        function() require('nvim-treesitter-textobjects.select').select_textobject('@function.inner', 'textobjects') end
-      )
-      vim.keymap.set(
-        { 'x', 'o' },
-        'ac',
-        function() require('nvim-treesitter-textobjects.select').select_textobject('@class.outer', 'textobjects') end
-      )
-      vim.keymap.set(
-        { 'x', 'o' },
-        'ic',
-        function() require('nvim-treesitter-textobjects.select').select_textobject('@class.inner', 'textobjects') end
-      )
-    end,
-  },
-  { 'folke/ts-comments.nvim', config = true, event = 'VeryLazy' },
-  {
-    'Wansmer/treesj',
-    event = 'VeryLazy',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    keys = { '<space>m', '<space>j', '<space>s' },
-    opts = {
-      use_default_keymaps = true,
-      check_syntax_error = true, -- try setting this false
-      max_join_length = 240,
-      ---Cursor behavior:
-      ---hold - cursor follows the node/place on which it was called
-      ---start - cursor jumps to the first symbol of the node being formatted
-      ---end - cursor jumps to the last symbol of the node being formatted
-      ---@type 'hold'|'start'|'end'
-      cursor_behavior = 'hold',
-      ---@type boolean Notify about possible problems or not
-      notify = true,
-      ---@type boolean Use `dot` for repeat action
-      dot_repeat = true,
-      ---@type nil|function Callback for treesj error handler. func (err_text, level, ...other_text)
-      on_error = nil,
-    },
-  },
-  {
-    'echasnovski/mini.ai',
-    version = '*',
-    event = 'VeryLazy',
-    config = true,
-    -- config = function()
-    --   local ts_spec = require('mini.ai').gen_spec.treesitter
-    --
-    --   return {
-    --     n_lines = 500,
-    --     custom_textobjects = {
-    --       -- argument
-    --       a = ts_spec { a = '@parameter.outer', i = '@parameter.inner' },
-    --       -- class
-    --       C = ts_spec { a = '@class.outer', i = '@class.inner' },
-    --       -- function call
-    --       f = ts_spec { a = '@call.outer', i = '@call.inner' },
-    --       -- definition
-    --       d = ts_spec {
-    --         a = { '@function.outer', '@class.outer' },
-    --         i = { '@function.inner', '@class.inner' },
-    --       },
-    --       -- scope
-    --       s = ts_spec {
-    --         a = { '@block.outer', '@conditional.outer', '@loop.outer' },
-    --         i = { '@block.inner', '@conditional.inner', '@loop.inner' },
-    --       },
-    --       -- loop
-    --       l = ts_spec { a = '@loop.outer', i = '@loop.inner' },
-    --     },
-    --   }
-    -- end,
-  },
-}
+vim.pack.add { 'gh:nvim-treesitter/nvim-treesitter', 'gh:OXY2DEV/markview.nvim' }
+
+require 'markview'.setup()
+require 'nvim-treesitter'.setup()
+
+  -- {
+  --   'nvim-treesitter/nvim-treesitter',
+  --   dependencies = { 'OXY2DEV/markview.nvim' }, -- markview provides treesitter queries and must be loaded first
+  --   branch = 'main',
+  --   build = ':TSUpdate',
+  --   opts = {
+  --     ensure_installed = 'all',
+  --     highlight = { enable = true },
+  --     indent = { enable = true },
+  --   },
+  -- },
+  -- {
+  --   'nvim-treesitter/nvim-treesitter-textobjects',
+  --   branch = 'main',
+  --   dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  --   lazy = false, -- this may not be able to be lazy loaded
+  --   -- event = { 'VeryLazy' },
+  --   opts = {
+  --     select = {
+  --       lookahead = true,
+  --       selection_modes = {
+  --         ['@parameter.outer'] = 'v', -- charwise
+  --         ['@function.outer'] = 'V', -- linewise
+  --         ['@class.outer'] = '<c-v>', -- blockwise
+  --       },
+  --       include_surrounding_whitespace = false,
+  --     },
+  --   },
+  --   config = function()
+  --     vim.keymap.set(
+  --       { 'x', 'o' },
+  --       'af',
+  --       function() require('nvim-treesitter-textobjects.select').select_textobject('@function.outer', 'textobjects') end
+  --     )
+  --     vim.keymap.set(
+  --       { 'x', 'o' },
+  --       'if',
+  --       function() require('nvim-treesitter-textobjects.select').select_textobject('@function.inner', 'textobjects') end
+  --     )
+  --     vim.keymap.set(
+  --       { 'x', 'o' },
+  --       'ac',
+  --       function() require('nvim-treesitter-textobjects.select').select_textobject('@class.outer', 'textobjects') end
+  --     )
+  --     vim.keymap.set(
+  --       { 'x', 'o' },
+  --       'ic',
+  --       function() require('nvim-treesitter-textobjects.select').select_textobject('@class.inner', 'textobjects') end
+  --     )
+  --   end,
+  -- },
+  -- { 'folke/ts-comments.nvim', config = true, event = 'VeryLazy' },
+  -- {
+  --   'Wansmer/treesj',
+  --   event = 'VeryLazy',
+  --   dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  --   keys = { '<space>m', '<space>j', '<space>s' },
+  --   opts = {
+  --     use_default_keymaps = true,
+  --     check_syntax_error = true, -- try setting this false
+  --     max_join_length = 240,
+  --     ---Cursor behavior:
+  --     ---hold - cursor follows the node/place on which it was called
+  --     ---start - cursor jumps to the first symbol of the node being formatted
+  --     ---end - cursor jumps to the last symbol of the node being formatted
+  --     ---@type 'hold'|'start'|'end'
+  --     cursor_behavior = 'hold',
+  --     ---@type boolean Notify about possible problems or not
+  --     notify = true,
+  --     ---@type boolean Use `dot` for repeat action
+  --     dot_repeat = true,
+  --     ---@type nil|function Callback for treesj error handler. func (err_text, level, ...other_text)
+  --     on_error = nil,
+  --   },
+  -- },
+  -- {
+  --   'echasnovski/mini.ai',
+  --   version = '*',
+  --   event = 'VeryLazy',
+  --   config = true,
+  --   -- config = function()
+  --   --   local ts_spec = require('mini.ai').gen_spec.treesitter
+  --   --
+  --   --   return {
+  --   --     n_lines = 500,
+  --   --     custom_textobjects = {
+  --   --       -- argument
+  --   --       a = ts_spec { a = '@parameter.outer', i = '@parameter.inner' },
+  --   --       -- class
+  --   --       C = ts_spec { a = '@class.outer', i = '@class.inner' },
+  --   --       -- function call
+  --   --       f = ts_spec { a = '@call.outer', i = '@call.inner' },
+  --   --       -- definition
+  --   --       d = ts_spec {
+  --   --         a = { '@function.outer', '@class.outer' },
+  --   --         i = { '@function.inner', '@class.inner' },
+  --   --       },
+  --   --       -- scope
+  --   --       s = ts_spec {
+  --   --         a = { '@block.outer', '@conditional.outer', '@loop.outer' },
+  --   --         i = { '@block.inner', '@conditional.inner', '@loop.inner' },
+  --   --       },
+  --   --       -- loop
+  --   --       l = ts_spec { a = '@loop.outer', i = '@loop.inner' },
+  --   --     },
+  --   --   }
+  --   -- end,
+  -- },
