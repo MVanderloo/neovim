36da774aa9966e6036f0de68433628604b59ef90
Michael van der Loo <michaeljvanderloo@gmail.com>
2025-08-06 23:30:39 -0400

    get some plugins working

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,7 +1,6 @@
-return {
-  {
-    'stevearc/oil.nvim',
-    config = true,
-    dependencies = { 'echasnovski/mini.icons' },
-  },
-}
+vim.pack.add { 'gh:stevearc/oil.nvim', 'gh:echasnovski/mini.icons' }
+
+require 'mini.icons'.setup {}
+require 'oil'.setup {}
+
+vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,80 +1,77 @@
+vim.pack.add { 'gh:nvim-lualine/lualine.nvim' }
+
 local function macro_recording()
   local reg = vim.fn.reg_recording()
   if reg == '' then return '' end
   return '@' .. reg
 end
 
-return {
-  {
-    'nvim-lualine/lualine.nvim',
-    event = 'UIEnter',
-    opts = {
-      refresh = { statusline = 10, tabline = 10, winbar = 10 },
-      options = {
-        component_separators = { left = '|', right = '|' },
-        section_separators = { left = '', right = '' },
-        -- section_separators = { left = '', right = '' },
-      },
-      sections = {
-        lualine_a = { 'mode', 'selectioncount', macro_recording },
-        lualine_b = {
-          { 'filename', path = 4 },
-        },
-        lualine_c = { 'diff' },
-        lualine_x = {
-          'searchcount',
-          {
-            'diagnostics',
-            symbols = { error = '󰅙 ', warn = '󰀦 ', info = '󰋼 ', hint = '󰌵 ' },
-          },
-        },
-        lualine_y = { 'progress', 'filesize' },
-        lualine_z = { 'location' },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-        lualine_y = {},
-        lualine_z = {},
-      },
-      -- tabline = {
-      --   lualine_a = {},
-      --   lualine_b = {},
-      --   lualine_c = {},
-      --   lualine_x = {},
-      --   lualine_y = {},
-      --   lualine_z = {},
-      -- },
-      -- winbar = {
-      --   lualine_a = {},
-      --   lualine_b = {},
-      --   lualine_c = { 'filename' },
-      --   lualine_x = {},
-      --   lualine_y = {},
-      --   lualine_z = {},
-      -- },
-      -- inactive_winbar = {},
-      -- extensions = {},
-    },
+require('lualine').setup {
+  refresh = { statusline = 10, tabline = 10, winbar = 10 },
+  options = {
+    component_separators = { left = '|', right = '|' },
+    section_separators = { left = '', right = '' },
+    -- section_separators = { left = '', right = '' },
   },
-  {
-    'jake-stewart/auto-cmdheight.nvim',
-    lazy = false,
-    opts = {
-      -- max cmdheight before displaying hit enter prompt.
-      max_lines = 5,
-
-      -- number of seconds until the cmdheight can restore.
-      duration = 2,
-
-      -- whether key press is required to restore cmdheight.
-      remove_on_key = true,
-
-      -- always clear the cmdline after duration and key press.
-      -- by default it will only happen when cmdheight changed.
-      clear_always = false,
+  sections = {
+    lualine_a = { 'mode', 'selectioncount', macro_recording },
+    lualine_b = {
+      { 'filename', path = 4 },
     },
+    lualine_c = { 'diff' },
+    lualine_x = {
+      'searchcount',
+      {
+        'diagnostics',
+        symbols = { error = '󰅙 ', warn = '󰀦 ', info = '󰋼 ', hint = '󰌵 ' },
+      },
+    },
+    lualine_y = { 'progress', 'filesize' },
+    lualine_z = { 'location' },
   },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  -- tabline = {
+  --   lualine_a = {},
+  --   lualine_b = {},
+  --   lualine_c = {},
+  --   lualine_x = {},
+  --   lualine_y = {},
+  --   lualine_z = {},
+  -- },
+  -- winbar = {
+  --   lualine_a = {},
+  --   lualine_b = {},
+  --   lualine_c = { 'filename' },
+  --   lualine_x = {},
+  --   lualine_y = {},
+  --   lualine_z = {},
+  -- },
+  -- inactive_winbar = {},
+  -- extensions = {},
 }
+--
+-- {
+--   'jake-stewart/auto-cmdheight.nvim',
+--   lazy = false,
+--   opts = {
+--     -- max cmdheight before displaying hit enter prompt.
+--     max_lines = 5,
+--
+--     -- number of seconds until the cmdheight can restore.
+--     duration = 2,
+--
+--     -- whether key press is required to restore cmdheight.
+--     remove_on_key = true,
+--
+--     -- always clear the cmdline after duration and key press.
+--     -- by default it will only happen when cmdheight changed.
+--     clear_always = false,
+--   },
+-- },
