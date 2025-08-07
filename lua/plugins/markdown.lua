ca557e3f559b4960827c9bc25ba2c66e2dcee8ba
Michael van der Loo <github@mvanderloo.com>
2025-08-07 11:07:41 -0400

    comment out these until ready

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,21 +1,21 @@
-return {
-  -- {
-  --   'MeanderingProgrammer/render-markdown.nvim',
-  --   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
-  --   ftype = { 'markdown' },
-  --   opts = {
-  --     -- completions was causing some error while i was not in markdown
-  --     -- completions = { blink = { enabled = true } }
-  --   },
-  -- },
-  {
-    'OXY2DEV/markview.nvim',
-    lazy = false,
-    dependencies = { 'saghen/blink.cmp' },
-    opts = { experimental = { check_rtp_message = false } },
-  },
-  {
-    'OXY2DEV/helpview.nvim',
-    lazy = false,
-  },
-}
+-- return {
+--   -- {
+--   --   'MeanderingProgrammer/render-markdown.nvim',
+--   --   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
+--   --   ftype = { 'markdown' },
+--   --   opts = {
+--   --     -- completions was causing some error while i was not in markdown
+--   --     -- completions = { blink = { enabled = true } }
+--   --   },
+--   -- },
+--   {
+--     'OXY2DEV/markview.nvim',
+--     lazy = false,
+--     dependencies = { 'saghen/blink.cmp' },
+--     opts = { experimental = { check_rtp_message = false } },
+--   },
+--   {
+--     'OXY2DEV/helpview.nvim',
+--     lazy = false,
+--   },
+-- }
