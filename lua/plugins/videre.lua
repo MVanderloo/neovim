6a8baecec025ffbec9d5b626ee3fad29908ad61f
Michael van der Loo <github@mvanderloo.com>
2025-08-10 18:52:41 -0400

    videre plugin

---
+++ lua/plugins/videre.lua
@@ -0,0 +1,13 @@
+vim.pack.add {
+  'gh:owen-dechow/videre.nvim',
+  'gh:Owen-Dechow/nvim_json_graph_view',
+  'gh:Owen-Dechow/graph_view_yaml_parser', -- Optional: add YAML support
+  'gh:Owen-Dechow/graph_view_toml_parser', -- Optional: add TOML support
+  -- 'gh:a-usr/xml2lua.nvim', -- Optional | Experimental: add XML support
+}
+
+require('videre').setup({
+  round_units = false,
+  keymap_desc_deliminator = "->",
+  link_backward = "H",
+})
