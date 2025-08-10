318913c09df9a65e81ed0b4cf5ca838084f7a90b
Michael van der Loo <github@mvanderloo.com>
2025-08-09 21:22:04 -0400

    vim tmux navigator

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,19 +1,7 @@
-return {
-  'christoomey/vim-tmux-navigator',
-  event = 'VeryLazy',
-  cmd = {
-    'TmuxNavigateLeft',
-    'TmuxNavigateDown',
-    'TmuxNavigateUp',
-    'TmuxNavigateRight',
-    'TmuxNavigatePrevious',
-    'TmuxNavigatorProcessList',
-  },
-  keys = {
-    { '<C-h>', '<cmd>TmuxNavigateLeft("n")<cr>', mode = { 'n', 'v', 'i' }, desc = 'TmuxNavigateLeft' },
-    { '<C-j>', '<cmd>TmuxNavigateDown<cr>', mode = { 'n', 'v', 'i' }, desc = 'TmuxNavigateDown' },
-    { '<C-k>', '<cmd>TmuxNavigateUp<cr>', mode = { 'n', 'v', 'i' }, desc = 'TmuxNavigateUp' },
-    { '<C-l>', '<cmd>TmuxNavigateRight<cr>', mode = { 'n', 'v', 'i' }, desc = 'TmuxNavigateRight' },
-    { '<C-\\>', '<cmd>TmuxNavigatePrevious<cr>', mode = { 'n', 'v', 'i' }, desc = 'TmuxNavigatePrevious' },
-  },
-}
+vim.pack.add { 'gh:christoomey/vim-tmux-navigator' }
+
+vim.keymap.set({ 'n', 'x', 'i' }, '<C-h>', '<cmd>TmuxNavigateLeft<cr>', { remap = false })
+vim.keymap.set({ 'n', 'x', 'i' }, '<C-j>', '<cmd>TmuxNavigateDown<cr>', { remap = false })
+vim.keymap.set({ 'n', 'x', 'i' }, '<C-k>', '<cmd>TmuxNavigateUp<cr>', { remap = false })
+vim.keymap.set({ 'n', 'x', 'i' }, '<C-l>', '<cmd>TmuxNavigateRight<cr>', { remap = false })
+vim.keymap.set({ 'n', 'x', 'i' }, '<C-\\>', '<cmd>TmuxNavigatePrevious<cr>', { remap = false })
