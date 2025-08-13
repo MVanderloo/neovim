c6b7a0d1dbb5c2d713cc75627aae1c60a78c8c1b
Michael van der Loo <github@mvanderloo.com>
2025-08-13 17:34:41 -0400

    lsp config WIP

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,18 +1,125 @@
-local methods = vim.lsp.protocol.Methods
+-- settings to merge with every LSP config
+-- vim.lsp.config('*', {
+--   capabilities = {
+--     textDocument = {
+--       semanticTokens = {
+--         multilineTokenSupport = true,
+--       },
+--     },
+--   },
+--   root_markers = { '.git', '.nvim.lua' },
+-- })
 
-local M = {}
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'Configure LSP keymaps',
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
+    if client:supports_method 'textDocument/definition' then
+      vim.keymap.set({ 'n' }, 'gd', function() vim.lsp.buf.definition() end)
+    end
+
+    if client:supports_method 'textDocument/references' then
+      vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end)
+    end
+
+    if client:supports_method 'textDocument/typeDefinition' then
+      vim.keymap.set({ 'n' }, 'gy', function() vim.lsp.buf.type_definition() end)
+    end
+
+    if client:supports_method 'textDocument/implementation' then
+      vim.keymap.set({ 'n' }, 'gli', function() vim.lsp.buf.implementation() end)
+    end
+
+    if client:supports_method 'textDocument/codeAction' then
+      vim.keymap.set({ 'n' }, 'gla', function() vim.lsp.buf.code_action() end)
+    end
+
+    if client:supports_method 'textDocument/rename' then
+      vim.keymap.set('n', 'glr', function() vim.lsp.buf.rename() end)
+    end
+
+    if client:supports_method 'textDocument/hover' then
+      vim.keymap.set({ 'n', 'x' }, 'K', function() vim.lsp.buf.hover() end)
+    end
+
+    if client:supports_method 'textDocument/documentColor' then vim.lsp.document_color.enable(true, args.buf) end
+
+    -- if client:supports_method 'textDocument/signatureHelp' then
+    --   vim.keymap.set({ 'i' }, '<C-s>', function() vim.lsp.buf.signature_help() end, {})
+    -- end
+
+    -- if client:supports_method(methods.textDocument_documentHighlight) then
+    --   vim.api.nvim_create_autocmd({ 'CursorHold', 'InsertLeave' }, {
+    --     desc = 'Highlight references under the cursor',
+    --     buffer = bufnr,
+    --     callback = vim.lsp.buf.document_highlight,
+    --   })
+    --   vim.api.nvim_create_autocmd({ 'CursorMoved', 'InsertEnter', 'BufLeave' }, {
+    --     desc = 'Clear highlight references',
+    --     buffer = bufnr,
+    --     callback = vim.lsp.buf.clear_references,
+    --   })
+    -- end
+
+    -- if client:supports_method(methods.textDocument_inlayHint) and vim.g.inlay_hints then
+    --   -- Initial inlay hint display.
+    --   vim.defer_fn(function()
+    --     local mode = vim.api.nvim_get_mode().mode
+    --     vim.lsp.inlay_hint.enable(mode == 'n' or mode == 'v', { bufnr = bufnr })
+    --   end, 500)
+    --
+    --   vim.api.nvim_create_autocmd('InsertEnter', {
+    --     desc = 'Enable inlay hints',
+    --     buffer = bufnr,
+    --     callback = function()
+    --       if vim.g.inlay_hints then vim.lsp.inlay_hint.enable(false, { bufnr = bufnr }) end
+    --     end,
+    --   })
+    --
+    --   vim.api.nvim_create_autocmd('InsertLeave', {
+    --     desc = 'Disable inlay hints',
+    --     buffer = bufnr,
+    --     callback = function()
+    --       if vim.g.inlay_hints then vim.lsp.inlay_hint.enable(true, { bufnr = bufnr }) end
+    --     end,
+    --   })
+    -- end
+  end,
+})
+
+-- Set up LSP servers.
+-- vim.api.nvim_create_autocmd({ 'BufReadPre', 'BufNewFile' }, {
+--   once = true,
+--   callback = function()
+--     local server_configs = vim
+--       .iter(vim.api.nvim_get_runtime_file('lsp/*.lua', true))
+--       :map(function(file) return vim.fn.fnamemodify(file, ':t:r') end)
+--       :totable()
+--     vim.lsp.enable(server_configs)
+--   end,
+-- })
+
+vim.lsp.enable {
+  'awk_ls',
+  'basedpyright',
+  'bashls',
+  'jsonls',
+  'lua_ls',
+  'pyrefly',
+  'ruff',
+  'rust_analyzer',
+  'taplo',
+  'ty',
+  'yamlls',
+}
+
+-- old stuff
 -- disable inlay hints by default
-vim.g.inlay_hints = false
+-- vim.g.inlay_hints = false
 
 --- Sets up LSP keymaps and autocommands for the given buffer.
----@param client vim.lsp.Client
----@param bufnr integer
 -- local function on_attach(client, bufnr)
---   ---@param lhs string
---   ---@param rhs string|function
---   ---@param desc string
---   ---@param mode? string|string[]
 --   local function keymap(lhs, rhs, desc, mode)
 --     mode = mode or 'n'
 --     vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
@@ -119,74 +226,3 @@ vim.g.inlay_hints = false
 --
 --   return vim.lsp.handlers[methods.client_registerCapability](err, res, ctx)
 -- end
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'Configure LSP keymaps',
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-
-    if client:supports_method 'textDocument/definition' then
-      vim.keymap.set({ 'n' }, 'gd', function() vim.lsp.buf.definition() end)
-    end
-
-    if client:supports_method 'textDocument/references' then
-      vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end)
-    end
-
-    if client:supports_method 'textDocument/typeDefinition' then
-      vim.keymap.set({ 'n' }, 'gy', function() vim.lsp.buf.type_definition() end)
-    end
-
-    if client:supports_method 'textDocument/implementation' then
-      vim.keymap.set({ 'n' }, 'gli', function() vim.lsp.buf.implementation() end)
-    end
-
-    if client:supports_method 'textDocument/codeAction' then
-      vim.keymap.set({ 'n' }, 'gla', function() vim.lsp.buf.code_action() end)
-    end
-
-    if client:supports_method 'textDocument/rename' then
-      vim.keymap.set('n', 'glr', function() vim.lsp.buf.rename() end)
-    end
-
-    if client:supports_method 'textDocument/hover' then
-      vim.keymap.set({ 'n', 'x' }, 'K', function() vim.lsp.buf.hover() end)
-    end
-
-    if client:supports_method('textDocument/documentColor') then
-      vim.lsp.document_color.enable(true, args.buf)
-    end
-
-    -- if client:supports_method 'textDocument/signatureHelp' then
-    --   vim.keymap.set({ 'i' }, '<C-s>', function() vim.lsp.buf.signature_help() end, {})
-    -- end
-  end,
-})
-
--- Set up LSP servers.
--- vim.api.nvim_create_autocmd({ 'BufReadPre', 'BufNewFile' }, {
---   once = true,
---   callback = function()
---     local server_configs = vim
---       .iter(vim.api.nvim_get_runtime_file('lsp/*.lua', true))
---       :map(function(file) return vim.fn.fnamemodify(file, ':t:r') end)
---       :totable()
---     vim.lsp.enable(server_configs)
---   end,
--- })
-
-vim.lsp.enable {
-  'awk_ls',
-  'basedpyright',
-  'bashls',
-  'jsonls',
-  'lua_ls',
-  'pyrefly',
-  'ruff',
-  'rust_analyzer',
-  'taplo',
-  'ty',
-  'yamlls',
-}
-
-return M
