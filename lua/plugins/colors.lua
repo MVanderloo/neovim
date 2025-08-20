fd72aaaec342279b16092ae2a3155c4a0f23cb15
Michael van der Loo <github@mvanderloo.com>
2025-08-20 14:28:41 -0400

    clean up

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,44 +1,9 @@
--- disable builtin color schemes
-vim.opt.wildignore:append {
-  'blue.vim',
-  'darkblue.vim',
-  'delek.vim',
-  'desert.vim',
-  'elflord.vim',
-  'evening.vim',
-  'habamax.vim',
-  'industry.vim',
-  'koehler.vim',
-  'lunaperche.vim',
-  'morning.vim',
-  'murphy.vim',
-  'pablo.vim',
-  'peachpuff.vim',
-  'quiet.vim',
-  'retrobox.vim',
-  'ron.vim',
-  'shine.vim',
-  'slate.vim',
-  'sorbet.vim',
-  'torte.vim',
-  'unokai.vim',
-  'vim.vim',
-  'wildcharm.vim',
-  'zaibatsu.vim',
-  'zellner.vim',
-}
+vim.pack.add({
+  'gh:gbprod/nord.nvim',
+  -- { src = 'gh:catppuccin/nvim', name = 'catppuccin' },
+  -- 'gh:folke/tokyonight.nvim',
+  -- 'gh:navarasu/onedark.nvim',
+  -- { src = 'gh:rose-pine/neovim', name = 'rose-pine' },
+}, { confirm = false })
 
-vim.pack.add({ 'gh:shaunsingh/nord.nvim' }, { confirm = false })
 vim.cmd 'colorscheme nord'
-
--- vim.pack.add({ 'gh:catppuccin/nvim' }, { confirm = false })
--- vim.cmd 'colorscheme catppuccin'
-
--- vim.pack.add({ 'gh:folke/tokyonight.nvim' }, { confirm = false })
--- vim.cmd 'colorscheme tokyonight'
-
--- vim.pack.add({ 'gh:navarasu/onedark.nvim' }, { confirm = false })
--- vim.cmd 'colorscheme onedark'
-
--- vim.pack.add({ 'gh:rose-pine/neovim' }, { confirm = false })
--- vim.cmd 'colorscheme rose-pine'
